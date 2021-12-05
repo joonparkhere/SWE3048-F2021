ebc32dad528b20605dcc2891b3572d73b03196b4
HyeonjinChoi <63872787+HyeonjinChoi@users.noreply.github.com>
2021-12-05 21:58:07 +0900

    feature: Add data of board transfer object

---
+++ src/main/java/edu/skku/wpl2021f/board/BoardDTO.java
@@ -0,0 +1,62 @@
+package edu.skku.wpl2021f.board;
+
+public class BoardDTO {
+
+	int boardID;
+	int boardIdentity;
+	String boardTitle;
+	String userID;
+	String userNickname;
+	String boardDate;
+	String boardContent;
+	int boardAvailable;
+	
+	public int getBoardID() {
+		return boardID;
+	}
+	public void setBoardID(int boardID) {
+		this.boardID = boardID;
+	}
+	public int getBoardIdentity() {
+		return boardIdentity;
+	}
+	public void setBoardIdentity(int boardIdentity) {
+		this.boardIdentity = boardIdentity;
+	}
+	public String getBoardTitle() {
+		return boardTitle;
+	}
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserNickname() {
+		return userNickname;
+	}
+	public void setUserNickname(String userNickname) {
+		this.userNickname = userNickname;
+	}
+	public String getBoardDate() {
+		return boardDate;
+	}
+	public void setBoardDate(String boardDate) {
+		this.boardDate = boardDate;
+	}
+	public String getBoardContent() {
+		return boardContent;
+	}
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
+	}
+	public int getBoardAvailable() {
+		return boardAvailable;
+	}
+	public void setBoardAvailable(int boardAvailable) {
+		this.boardAvailable = boardAvailable;
+	}
+}
