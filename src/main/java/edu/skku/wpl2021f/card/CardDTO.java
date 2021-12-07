c1d9e02dbd43222fde2c3417355737312192983d
HyeonjinChoi <63872787+HyeonjinChoi@users.noreply.github.com>
2021-12-08 04:32:10 +0900

    feature: Add data of card transfer object

---
+++ src/main/java/edu/skku/wpl2021f/card/CardDTO.java
@@ -0,0 +1,34 @@
+package edu.skku.wpl2021f.card;
+
+public class CardDTO {
+	
+	int boardID;
+	int boardIdentity;
+	String cardTitle;
+	String cardContent;
+	
+	public int getBoardID() {
+		return boardID;
+	}
+	public void setBoardID(int boardID) {
+		this.boardID = boardID;
+	}
+	public int getBoardIdentity() {
+		return boardIdentity;
+	}
+	public void setBoardIdentity(int boardIdentity) {
+		this.boardIdentity = boardIdentity;
+	}
+	public String getCardTitle() {
+		return cardTitle;
+	}
+	public void setCardTitle(String cardTitle) {
+		this.cardTitle = cardTitle;
+	}
+	public String getCardContent() {
+		return cardContent;
+	}
+	public void setCardContent(String cardContent) {
+		this.cardContent = cardContent;
+	}
+}
