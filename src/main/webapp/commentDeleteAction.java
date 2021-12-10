6e22b5c28184abd041ffc68db9e712381e81dae4
HyeonjinChoi <63872787+HyeonjinChoi@users.noreply.github.com>
2021-12-10 18:13:56 +0900

    feature: Add commentDeleteAction

---
+++ src/main/webapp/commentDeleteAction.java
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import="edu.skku.wpl2021f.comment.CommentDAO"%>
+<%@ page import="edu.skku.wpl2021f.comment.CommentDTO"%>
+<%@ page import="java.io.PrintWriter"%>
+<%
+	request.setCharacterEncoding("UTF-8");
+	int boardID = Integer.parseInt(request.getParameter("boardID"));
+	int boardIdentity = Integer.parseInt(request.getParameter("boardIdentity"));
+	int commentID = Integer.parseInt(request.getParameter("commentID"));
+	
+	CommentDAO comment = new CommentDAO();
+	int result = comment.delete(boardID, boardIdentity, commentID);
+	if (result == -1) {
+		PrintWriter script = response.getWriter();
+		script.println("<script>");
+		script.println("alert('Failed!');");
+		script.println("history.back();");
+		script.println("</script>");
+		script.close();
+		return;
+	}
+	else {
+		PrintWriter script = response.getWriter();
+		script.println("<script>");
+		script.println("location.href = 'view.jsp?boardID=" + boardID + "&boardIdentity=" + boardIdentity + "';");
+		script.println("</script>");
+		script.close();
+		return;
+	}
+%>
